c155838599195efcdd6c2e93f89a6ecf87dfdfb7
Kartik <kartikl@yahoo.com>
2023-09-24 15:45:14 -0700

    Metrics' collection API

--- src/truenil/api/model/agent/models.py
+++ src/truenil/api/model/agent/models.py
@@ -4,6 +4,30 @@ from truenil.api.model.utils import PeeweeGetterDict
 from typing import Any, List, Optional
 
 
+class ReportedAgentMetrics(BaseModel):
+    """
+    metric_name is one of the below. Exact case
+        cpu_usage
+        number_of_vcpu
+        max_memory
+        memory_used
+        disk_space
+        free_space
+        network_connectivity
+        response_time
+        error_rate
+    """
+    metric_name : str
+    metric_value : float
+    # Optional field
+    process_name : str | None = None
+
+
+class MetricContainer(BaseModel):
+    agent_uuid: str
+    agent_metrics: list[ReportedAgentMetrics]
+
+
 class Agent(BaseModel):
     uuid: str
     version: str
@@ -15,3 +39,4 @@ class Agent(BaseModel):
     # Unstructured Metadata in JSON form. This would store OS information, and miscellaneous ones
     agent_metadata: Optional[Json[Any]]
     organization: str
+
